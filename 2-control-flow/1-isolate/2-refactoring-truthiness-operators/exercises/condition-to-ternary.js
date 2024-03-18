79e121033e94fbbb07045673304e3590e0c3ae22
ahlamboudali <hanane.boudali@gmail.com>
2024-03-18 10:49:59 +0100

    'add-control-flow

--- 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/condition-to-ternary.js
+++ 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/condition-to-ternary.js
@@ -7,17 +7,12 @@ console.log('-- begin --');
 // refactor this code to use a _?_:_ instead of a conditional
 
 // try different values and different types
-const isLoggedIn = _;
-const secretInformation = _;
-const warningMessage = _;
+const isLoggedIn = true;
+const secretInformation = 'sensitive data';
+const warningMessage = 'No secret information available';
 console.log(isLoggedIn, secretInformation, warningMessage);
 
-let toDisplay;
-if (isLoggedIn) {
-  toDisplay = secretInformation;
-} else {
-  toDisplay = warningMessage;
-}
+let toDisplay = isLoggedIn ? secretInformation : warningMessage
 console.log(toDisplay);
 
 console.log('-- end --');
